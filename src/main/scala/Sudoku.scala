c20e49cbeeba5a3cd5448d1e48c89de8764adbb6
karank85 <kkjas85@gmail.com>
2022-08-24 01:07:14 +0700

    implemented parseBoard and parseGrid

--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -18,9 +18,28 @@ object Sudoku extends App {
     val mpUnits = Map() ++ allSquares.map(sq => (sq,unitPeers.filter(_.contains(sq))))
     val mpPeers = Map() ++ mpUnits.map(e => (e._1, e._2.flatten.toSet - e._1))
 
+    def parseBoard: Map[String, String] = {
+      val boardVec = board.map(b => b.toString).toVector
+      allSquares.zip(boardVec).toMap
+    }
+
+    def parseGrid: Map[String, String] = {
+
+      val mp = allSquares.map(sq => sq -> "123456789").toMap
+      //val filtered = parseBoard.filter((s ,d) => "123456789".contains(d))
+      val filtered = parseBoard.filter(d => "123456789".contains(d._2) && !assign(mp, d._1, d._2))
+      if filtered.size != parseBoard.size then throw Exception("Can't assign") else mp
+
+    }
+
+    def assign(values: Map[String, String], s: String, d: String): Boolean = ???
+
     Vector()
 
   }
+
+  solve("003020600900305001001806400008102900700000008006708200002609500800203009005010300")
+  solve("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
 
 
 }
